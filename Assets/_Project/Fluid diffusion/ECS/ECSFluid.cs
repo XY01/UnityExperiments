6e0049fe71ffb61e8559d82e289aebdc5d986d38
BradRazor <brad@xy01.net>
2022-11-03 20:48:36 +1100

    ecs start

---
+++ Assets/_Project/Fluid diffusion/ECS/ECSFluid.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Unity.Collections;
+
+partial class FluidSystem : SystemBase
+{
+    private EndSimulationEntityCommandBufferSystem commandBufferSystem;
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        commandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+    protected override void OnUpdate()
+    {
+        //NativeArray<VoxDenisty> voxDensit = GetEntityQuery(typeof(VoxDenisty)).ToComponentDataArray<VoxDenisty>(Allocator.TempJob);
+
+        // DIFFUSE
+        //
+        Entities.ForEach
+        ((  ref VoxDenisty translation,
+            in VoxVector velocity) =>
+        {
+           
+        })
+        .Schedule();
+    }
+}
+
+public struct Voxel : IComponentData
+{
+    float3 pos;
+}
+
+public struct VoxDenisty : IComponentData
+{
+    float density;
+}
+
+public struct VoxVector : IComponentData
+{
+    float3 vec;
+}
