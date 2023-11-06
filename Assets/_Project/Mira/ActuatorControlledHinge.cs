95608b3ca13dc8bd3a56acd6bf0c39859bed8e7a
BradRazor <brad@xy01.net>
2023-11-06 21:02:23 +1100

    mira flower update

--- Assets/_Project/Mira/ActuatorControlledHinge.cs
+++ Assets/_Project/Mira/ActuatorControlledHinge.cs
@@ -7,26 +7,58 @@ using Random = Unity.Mathematics.Random;
 
 public class ActuatorControlledHinge : MonoBehaviour
 {
-    public float FixedLengthA = 1;
-    public float FixedLengthB = 2;
-    public float VariableLengthC = 1;
+    [SerializeField] private float HingeTipLengthFixed = 1;
+    [SerializeField] private float HingeBaseLengthFixed = 2;
+    private float BaseTipLength;
+    [SerializeField] float ActuatroLengthDynamic = 1;
     private float angle = 0;
+
+    [SerializeField] private Transform ActuatorBase;
+    [SerializeField] private Transform ActuatorTip;
+    [SerializeField] private Transform Hinge;
+
+    [SerializeField] private float angleOffset = 0;
+
+    [SerializeField] private float ActuatorAtRestLength = .7f;
+    [SerializeField] private float Stroke = .4f;
+
+    private float MinLength => ActuatorAtRestLength;
+    private float MaxLength => ActuatorAtRestLength + Stroke;
     
+    
+    private void OnValidate()
+    {
+    
+    }
+
     private void OnDrawGizmos()
     {
+        ActuatroLengthDynamic = Mathf.Clamp(ActuatroLengthDynamic,MinLength,MaxLength);
         
-        VariableLengthC = Mathf.Clamp(VariableLengthC, Mathf.Abs(FixedLengthA-FixedLengthB), FixedLengthA + FixedLengthB);
+        // Angle from base to hinge
+        float angleBaseToHinge = Vector3.SignedAngle(Vector3.down, (ActuatorBase.position - Hinge.position).normalized, Vector3.forward);
         
-        angle = CalculateAngle(FixedLengthA, FixedLengthB, VariableLengthC);
-        Vector3 vertAB = transform.position;
-        Vector3 vertBC = vertAB - new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad), Mathf.Cos(angle* Mathf.Deg2Rad), 0) * FixedLengthB;
-        Vector3 vertAC = vertAB + Vector3.down * FixedLengthA;
+        // Angle from hinge to actuator tip
+        angle = CalculateAngle(HingeTipLengthFixed,HingeBaseLengthFixed,ActuatroLengthDynamic);
+        angle -= angleBaseToHinge;
+        
+        Vector3 vertHinge = Hinge.position;
+        Vector3 vertActuatorTip = vertHinge - new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad), Mathf.Cos(angle* Mathf.Deg2Rad), 0) * HingeTipLengthFixed;
+        Vector3 vertActuatorBase = ActuatorBase.position;
+        
+        BaseTipLength = Vector3.Distance(vertActuatorTip, vertActuatorBase);
+
+        ActuatorTip.position = vertActuatorTip;
+        // Rotations 
+        ActuatorBase.localRotation = Quaternion.LookRotation(ActuatorBase.localPosition - vertActuatorTip);
+        Hinge.localRotation = Quaternion.LookRotation(Hinge.localPosition - vertActuatorTip);
+        
         // Length A
-        Gizmos.DrawLine(vertAB, vertAC);
+        Gizmos.DrawLine(vertHinge, vertActuatorBase);
         // Length B
-        Gizmos.DrawLine(vertAB, vertBC);
+        Gizmos.DrawLine(vertHinge, vertActuatorTip);
         // Length C
-        Gizmos.DrawLine(vertBC, vertAC);
+        Gizmos.DrawLine(vertActuatorTip, vertActuatorBase);
     }
 
     public static float CalculateAngle(float fixedLengthA, float fixedLengthB, float variableLengthC)
@@ -51,7 +83,11 @@ public class ActuatorControlledHinge : MonoBehaviour
 
             DrawDefaultInspector();
             
-            EditorGUILayout.LabelField("Angle", actuator.angle.ToString());
+            EditorGUILayout.LabelField("Angle", actuator.angle.ToString("F2"));
+            
+            EditorGUILayout.LabelField("Hinge - Base Length", actuator.HingeBaseLengthFixed.ToString("F2"));
+            EditorGUILayout.LabelField("Hinge - Tip Length", actuator.HingeTipLengthFixed.ToString("F2"));
+            EditorGUILayout.LabelField("Base - Tip Length", actuator.BaseTipLength.ToString("F2"));
             
             // myScript.myColor = EditorGUILayout.ColorField("My Color", myScript.myColor);
             //
